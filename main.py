291af77c2559cb7c423e7ffbb5124ea4f817e1f6
Ethan Chan <esamuelchan@gmail.com>
2024-10-10 09:31:27 -0700

    Format document

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ class Command:
 
         return None
 
+
 commands = Command()
+
 
 def get_executables_in_folder(folder_path):
     exec_names = []
@@ -36,11 +38,14 @@ def get_executables_in_folder(folder_path):
                 exec_names.append(file)
     return exec_names
 
+
 def close_application_by_exe(exe_name):
     for proc in psutil.process_iter():
         try:
             if proc.name().lower() == exe_name.lower():
-                handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, False, proc.pid)
+                handle = win32api.OpenProcess(
+                    win32con.PROCESS_TERMINATE, False, proc.pid
+                )
                 win32api.TerminateProcess(handle, 0)
                 win32api.CloseHandle(handle)
                 # print(f"{exe_name} terminated.")
@@ -48,16 +53,17 @@ def close_application_by_exe(exe_name):
 
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    
-        except (pywintypes.error):
+
+        except pywintypes.error:
             pass
 
     return False
 
+
 def monitor_executables(folder_path):
     global running
     executables = get_executables_in_folder(folder_path)
-    
+
     while running:
         # print("Monitoring and terminating executables...")
         for exe in executables:
@@ -69,7 +75,9 @@ def start_monitoring(folder_path):
     global running
     if not running:
         running = True
-        monitor_thread = threading.Thread(target=monitor_executables, args=(folder_path,))
+        monitor_thread = threading.Thread(
+            target=monitor_executables, args=(folder_path,)
+        )
         monitor_thread.start()
         print("Monitoring started...")
 
@@ -83,7 +91,7 @@ def stop_monitoring():
 
 def main():
     folder_path = r"C:/Program Files/DyKnow/Cloud/7.10.22.9"
-    
+
     while True:
         user_input = input(">>> ")
 
@@ -101,6 +109,7 @@ def main():
                 break
             case _:
                 print(f"Invalid input. Avaliable inputs:{commands.commands}")
+
 
 if __name__ == "__main__":
     main()
