8462fd2bf6576a55a21b2c8ca27b49fbf7401722
Ethan Chan <esamuelchan@gmail.com>
2024-11-02 20:30:57 -0700

    Fix second typo

--- api.cpp
+++ api.cpp
@@ -194,7 +194,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 }
             }
             break;
-    }
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 void stop_monitoring()
