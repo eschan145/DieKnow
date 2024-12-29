35316e4ee781abd5add7b1c499641f3573ecc828
Ethan Chan <esamuelchan@gmail.com>
2024-12-28 20:38:56 -0800

    Debugging

--- src/asteroids.cpp
+++ src/asteroids.cpp
@@ -78,7 +78,7 @@ void Asteroids::create(bool& flag) {
         "STATIC",
         "Something not working?",
         WS_VISIBLE | WS_CHILD,
-        161, this->rect.height - 9, this->rect.width, 18,
+        161, 50, this->rect.width, 18,
         this->hwnd,
         (HMENU)ID_HELP,
         wc.hInstance,
