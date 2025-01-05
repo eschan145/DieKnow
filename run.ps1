97e70d8dd6cc65b45d2bcf6fa96405472c77952e
eschan145 <esamuelchan@gmail.com>
2025-01-04 16:04:57 -0800

    Enhancements to start script

--- run.ps1
+++ run.ps1
@@ -1,12 +1,30 @@
 Write-Host "DieKnow - win32 x86_64"
 Write-Host "There is ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law."
 
+$architecture = if ([System.IntPtr]::Size -eq 8) { "64-bit" } else { "32-bit" }
+
+if ($architecture -ne "64-bit") {
+    Write-Host "FATAL: You are attempting to run a 64-bit program on a 32-bit system."
+    $global:LASTEXITCODE = 1
+    Exit
+}
+
+$python = Get-Command python -ErrorAction SilentlyContinue
+$py = Get-Command py -ErrorAction SilentlyContinue
+
+if (-not $python -and -not $py) {
+    Write-Host "FATAL: Python is not installed or not in PATH system environment variable!"
+    $global:LASTEXITCODE = 1
+    return
+}
+
 try {
-    & py src\main.py
-} catch {
-    try {
+    if ($py) {
+        & py src\main.py
+    } elseif ($python) {
         & python src\main.py
-    } catch {
-        Write-Host "FATAL: Both 'py' and 'python' command failed. Python may not be installed or in PATH system environment variable!"
     }
+} catch {
+    Write-Host "FATAL: Failed to run."
+    $global:LASTEXITCODE = 1
 }
