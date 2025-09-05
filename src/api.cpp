3732ba1e86b27a4ea023d14a8ffd23d99744ce17
Ethan Chan <esamuelchan@gmail.com>
2025-09-05 16:15:43 -0700

    Fix missing backslash resulting in invalid path

--- src/api.cpp
+++ src/api.cpp
@@ -24,7 +24,7 @@ Compile with g++ -shared -o api.dll api.cpp -Ofast -fPIC -shared
 
 #include "api.h"
 
-const char* SHORT_FOLDER_PATH = "C:\Program Files\\DyKnow\\Cloud";
+const char* SHORT_FOLDER_PATH = "C:\\Program Files\\DyKnow\\Cloud";
 const char* FOLDER_PATH = "C:\\Program Files\\DyKnow\\Cloud";
 
 const char* DYK_CLASS_NAME = "WindowsForms10.Window.8.app.0.9fe31_r7_ad1";
